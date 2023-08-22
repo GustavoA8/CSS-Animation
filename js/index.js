ed5091ebc537b27952666ebafab1e6a21794ffd9
GustavoA8 <129892376+GustavoA8@users.noreply.github.com>
2023-08-22 17:25:08 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+export  class Animacao{
+    constructor(){
+        this.cont=0
+    }
+
+
+    getContador(){
+        return this.cont
+    }
+    
+    addContador(){
+        this.cont++
+    }
+    subContador(){
+        this.cont--
+    }
+
+
+
+}
